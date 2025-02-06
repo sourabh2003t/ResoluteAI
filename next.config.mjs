bfcd03a93fe0ebb79d2c9623d6fbc964c2763d5f
sourabh2003t <106023151+sourabh2003t@users.noreply.github.com>
2025-02-06 10:37:39 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/Login",
+  basePath: "/login",
   reactStrictMode: true
-};
+}
 
-module.exports = nextConfig;
+export default nextConfig
